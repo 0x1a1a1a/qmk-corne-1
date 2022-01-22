90ed258d5064a2ca7550172910b9403b94a52d33
Ihor Kos <59264318+inununit@users.noreply.github.com>
2022-01-23 00:25:34 +0200

    Added defines for Home Row Mod-Taps

--- keymap.c
+++ keymap.c
@@ -1,6 +1,15 @@
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+#define HOME_J LSFT_T(KC_J)
+#define HOME_K LCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN LGUI_T(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
 
