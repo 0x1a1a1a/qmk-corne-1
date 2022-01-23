c9b9c8600a72de425a24f781cd6719de29a02594
inununit <59264318+inununit@users.noreply.github.com>
2022-01-24 01:58:36 +0200

    Returned MO to the left thumb and set to the right thumb also

--- keymap.c
+++ keymap.c
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
          XXXXXXX,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,            KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_QUOT,
          XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX,
-                                     KC_ENT,  KC_SPC,   TO(1),            TO(2), KC_BSPC, KC_TAB
+                                     KC_ENT,  KC_SPC,   MO(1),            MO(2), KC_BSPC, KC_TAB
 
     ),
 
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          XXXXXXX,   TAB_P,   TAB_N,   KC_UP,   ALFRD,  LANG_N,          LANG_P,  RBRC_L,  RBRC_R,  KC_GRV, KC_LBRC, KC_RBRC,
          XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,         KC_MINS,  HOME_J,  HOME_K,  HOME_L, HOME_SC, XXXXXXX,
          XXXXXXX, XXXXXXX, XXXXXXX,   SPC_L,   MCTRL,   SPC_R,          SS_OPT,  SS_C_A,  SS_C_S,  SS_F_A,  SS_F_S, XXXXXXX,
-                                    _______, _______,   TO(0),           TO(4), _______, _______
+                                    _______, _______, XXXXXXX,           TO(4), _______, _______
 
     ),
 
@@ -80,8 +80,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_MUTE, KC_VOLU, KC_VOLD, XXXXXXX, XXXXXXX,  UNDRSC,         KC_COMM,    KC_7,    KC_8,    KC_9, KC_PLUS, KC_MINS,
          XXXXXXX,  HOME_A,  HOME_S,  HOME_D,  HOME_F, KC_QUES,          KC_DOT,    KC_4,    KC_5,    KC_6, KC_ASTR, KC_SLSH,
          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EXLM,            KC_0,    KC_1,    KC_2,    KC_3, KC_BSLS,  KC_EQL,
-                                    _______, _______,   TO(3),           TO(0), _______, _______
- 
+                                    _______, _______,   TO(3),         XXXXXXX, _______, _______
+
     ),
 
     // System and RGB controls, additional special characters
