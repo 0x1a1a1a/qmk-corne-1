df5c0226ec7efc1f7b44fed6cd96d5c9ad25c115
inununit <59264318+inununit@users.noreply.github.com>
2022-01-23 11:49:20 +0200

    Minor fixes

--- keymap.c
+++ keymap.c
@@ -6,7 +6,6 @@ BACKLOG
 Next tab in Chrome: command + shift + close bracket
 Previous tab in Chrome: command + shift + open bracket
 Shortcuts for Zoom: mute audio, leave call (Command+W?)
-Screenshots on macOS
 Figma-specific layer
 
 */
@@ -19,7 +18,8 @@ Figma-specific layer
 
 
 
-// Home row mod-taps
+
+// Home row mods (mod-taps)
 #define HOME_A LCTL_T(KC_A)
 #define HOME_S LOPT_T(KC_S)
 #define HOME_D LSFT_T(KC_D)
@@ -62,30 +62,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     ),
 
-  [1] = LAYOUT_split_3x6_3(
+    [1] = LAYOUT_split_3x6_3(
 
              DND, KC_LBRC, KC_RBRC,   KC_UP, XXXXXXX,  LANG_P,          LANG_N,  RBRC_L,  RBRC_R, XXXXXXX, KC_LBRC, KC_RBRC,
-         XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,         XXXXXXX,  HOME_J,  HOME_K,  HOME_L, HOME_SC, XXXXXXX,
+         XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,         KC_MINS,  HOME_J,  HOME_K,  HOME_L, HOME_SC, XXXXXXX,
            ALFRD, XXXXXXX, XXXXXXX,   SPC_L,   MCTRL,   SPC_R,          SS_OPT,  SS_C_A,  SS_C_S,  SS_F_A,  SS_F_S, XXXXXXX,
-                                    XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX
+                                    _______, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, _______
 
-  ),
+    ),
 
-  [2] = LAYOUT_split_3x6_3(
+    [2] = LAYOUT_split_3x6_3(
 
           KC_GRV, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  UNDRSC,         KC_COMM,    KC_7,    KC_8,    KC_9, KC_PLUS, KC_MINS,
           KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F, KC_QUES,          KC_DOT,    KC_4,    KC_5,    KC_6, KC_ASTR, KC_SLSH,
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EXLM,            KC_0,    KC_1,    KC_2,    KC_3,  KC_EQL, KC_BSLS,
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_EXLM,            KC_0,    KC_1,    KC_2,    KC_3, KC_BSLS,  KC_EQL,
                                     _______, XXXXXXX,   TO(0),           TO(3), XXXXXXX, _______
  
-  ),
+    ),
 
-  [3] = LAYOUT_split_3x6_3(
+    [3] = LAYOUT_split_3x6_3(
 
            RESET, BL_TOGG, BL_STEP, BL_BRTG, XXXXXXX, XXXXXXX,          RBRC_L,  RBRC_R, XXXXXXX, KC_MINS,   NDASH,   MDASH,
          RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX,         KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
          RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX,         KC_LCBR, KC_RCBR, KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX,
                                     _______, XXXXXXX,   TO(0),         XXXXXXX, XXXXXXX, _______
 
-  )
+    )
 };
