24f926e3be3d8204423144dbc5afd4c4d3c4a80d
inununit <59264318+inununit@users.noreply.github.com>
2022-02-06 15:45:29 +0200

    Space optimization commands in rules.mk

--- rules.mk
+++ rules.mk
@@ -1,5 +1,18 @@
-RGBLIGHT_ENABLE = no
+APPLE_FN_ENABLE = yes
+AUDIO_ENABLE = no           # Save space: Audio output on port C6
+BACKLIGHT_ENABLE = no       # Save space: Backlight
+BLUETOOTH_ENABLE = no       # Save space: Bluetooth
+# BOOTLOADER = atmel-dfu    # Disabled until further investigation
+EXTRAFLAGS += -flto         # Save space: Enable Link Time Optimization (https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/)
+EXTRAKEY_ENABLE = no        # Save space: Disable Windows-only multimedia keys
+GRAVE_ESC_ENABLE = no       # Save space: Disabel Grave Escape (https://docs.qmk.fm/#/feature_grave_esc)
+MIDI_ENABLE = no            # Save space: Disable MIDI controls
+MOUSEKEY_ENABLE = no        # Save space: Disable mouse keys
+OLED_DRIVER_ENABLE = no     # Save space: Disable OLED
+
 RGB_MATRIX_ENABLE = yes
-MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = no
+SPACE_CADET_ENABLE = no     # Save space: Disable Space Cadet (https://docs.qmk.fm/#/feature_space_cadet)
+SWAP_HANDS_ENABLE = no      # Save space: One-hand typing
 TAP_DANCE_ENABLE = yes
 UNICODE_ENABLE = no
