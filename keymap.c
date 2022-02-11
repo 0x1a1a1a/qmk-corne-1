c9a9ad5b3a0cc3dc2b67f5d594594bc3157817a7
inununit <59264318+inununit@users.noreply.github.com>
2022-02-11 03:17:10 +0200

    Minor optimizations

--- keymap.c
+++ keymap.c
@@ -90,14 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
           KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,            KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_QUOT,
           KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
-                                    KC_MINS,  KC_SPC,   TO(1),           TO(2), KC_BSPC,   TO(3)
+                                    XXXXXXX,  KC_SPC,   TO(1),           TO(2), KC_BSPC,   TO(3)
 
     ),
 
     // ARROWS AND MACOS-SPECIFIC CONTROLS
     [1] = LAYOUT_split_3x6_3(
 
-          KC_ESC,   MCTRL,   SPC_L,   KC_UP,   SPC_R,   ALFRD,           ALFRD,     DND,   TAB_P,   TAB_N, XXXXXXX, KC_MUTE,
+          KC_ESC,   MCTRL,   SPC_L,   KC_UP,   SPC_R,   ALFRD,         KC_MINS,     DND,   TAB_P,   TAB_N, XXXXXXX, KC_MUTE,
           KC_ENT, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,           ONEPS,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_VOLU,
           KC_TAB,    CMDZ,    CMDX,    CMDC,    CMDV,    LANG,          SS_OPT,  SS_C_A,  SS_C_S,  SS_F_A,  SS_F_S, KC_VOLD,
                                       TO(0),  KC_SPC,   TO(0),           U_PRD, KC_BSPC,  U_PRDD
@@ -107,8 +107,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // NUMPAD   
     [2] = LAYOUT_split_3x6_3(
 
-          KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR,         KC_PLUS,    KC_7,    KC_8,    KC_9,  KC_DOT, XXXXXXX,
-          KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F, KC_SLSH,         KC_MINS,    KC_4,    KC_5,    KC_6, KC_COMM, XXXXXXX,
+          KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_ASTR,         KC_MINS,    KC_7,    KC_8,    KC_9,  KC_DOT, XXXXXXX,
+          KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F, KC_SLSH,         KC_PLUS,    KC_4,    KC_5,    KC_6, KC_COMM, XXXXXXX,
           KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_EQL,            KC_0,    KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,
                                       TO(0),  KC_SPC, XXXXXXX,           TO(0), KC_BSPC, XXXXXXX
 
