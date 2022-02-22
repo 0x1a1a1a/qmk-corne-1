5cb6ebe7015a8b0e0c532a4679d91885c8ac3e2b
inununit <59264318+inununit@users.noreply.github.com>
2022-02-22 18:43:33 +0200

    Set MO(3) instead of TO(3) and realign layers 3 and 4

--- keymap.c
+++ keymap.c
@@ -4,6 +4,7 @@
 /*
 
 Next steps
+      - Remap Alfred away from the thumb (thumbs indicating current layer should be empty to avoid double clicking
       - Implement Unicode support for special characters to use one layer for latin and cyrillic special characters
       - 
       - 
@@ -100,46 +101,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
           KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,            KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_QUOT,
           KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
-                                       LANG,  KC_SPC,   TO(1),           TO(2), KC_BSPC,   TO(3)
+                                    XXXXXXX,  KC_SPC,   TO(1),           MO(2), KC_BSPC,   TO(3)
 
     ),
 
     // ARROWS AND MACOS-SPECIFIC CONTROLS
     [1] = LAYOUT_split_3x6_3(
 
-          KC_ESC,     DND,   SPC_L,   KC_UP,   SPC_R,   MCTRL,            DSK1,    DSK2,    DSK3,    DSK4,    DSK5, KC_MUTE,
-          KC_ENT,  SS_OPT, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,         XXXXXXX,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_VOLU,
-          KC_TAB,    CMDZ,    CMDX,    CMDC,    CMDV,   ONEPS,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD,
-                                      TO(0),  KC_SPC,   ALFRD,           TO(2), KC_BSPC,   TO(3)
+          KC_ESC, XXXXXXX,   SPC_L,   KC_UP,   SPC_R,   ALFRD,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     DND, KC_MUTE,
+          KC_ENT,   MCTRL, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,         XXXXXXX,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_VOLU,
+          KC_TAB,    DSK1,    DSK2,    DSK3,    DSK4,    DSK5,         XXXXXXX, XXXXXXX, XXXXXXX,  SS_OPT,   ONEPS, KC_VOLD,
+                                      TO(0),  KC_SPC, XXXXXXX,           MO(2), KC_BSPC,   TO(3)
 
     ),
 
-    // NUMPAD       
+    // SPECIAL CHARACTERS
     [2] = LAYOUT_split_3x6_3(
+
+          KC_ESC,    DLLR, KC_LBRC,   ATSGN, KC_RBRC, KC_EXLM,        CYR_EXLM, KC_QUOT,  D_QUOT,   COLON, KC_SCLN,  KC_GRV,
+          KC_ENT,   AMPRD, RDBRC_L,    NMBR, RDBRC_R, KC_QUES,        CYR_QUES, KC_MINS,   NDASH,   MDASH,  CYR_QT, KC_SLSH,
+          KC_TAB,   CARET, KC_LCBR,   PRCNT, KC_RCBR,  UNDRSC,            LANG,  CYR_QT, CYR_DQT, CYR_CLN,CYR_SCLN, KC_BSLS,
+                                    XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX
+    ),
+
+    // NUMPAD       
+    [3] = LAYOUT_split_3x6_3(
 
           KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_7,    KC_8,    KC_9, KC_PLUS, KC_MINS,  KC_DOT,
           KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F, XXXXXXX,            KC_4,    KC_5,    KC_6, KC_ASTR, KC_SLSH, KC_COMM,
           KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,  KC_EQL,
-                                      TO(0),  KC_SPC,   TO(1),            KC_0, KC_BSPC,   TO(3)
+                                      TO(0),  KC_SPC,   TO(1),           MO(2), KC_BSPC,    KC_0
 
-    ),
-
-    // SPECIAL CHARACTERS: LATIN
-    [3] = LAYOUT_split_3x6_3(
-
-          KC_ESC,    DLLR, KC_LBRC,   ATSGN, KC_RBRC, KC_EXLM,          KC_GRV, KC_MINS,   NDASH,   MDASH, XXXXXXX, XXXXXXX,
-          KC_ENT,   AMPRD, RDBRC_L,    NMBR, RDBRC_R, KC_QUES,           COLON, KC_QUOT, KC_SLSH, XXXXXXX, XXXXXXX, XXXXXXX,
-          KC_TAB,   CARET, KC_LCBR,   PRCNT, KC_RCBR,  UNDRSC,         KC_SCLN,  D_QUOT, KC_BSLS, XXXXXXX, XXXXXXX, XXXXXXX,
-                                      TO(0),  KC_SPC,   TO(1),           TO(2), KC_BSPC,   TO(4)
-    ),
-
-    // SPECIAL CHARACTERS: CYRILLIC
-    [4] = LAYOUT_split_3x6_3(
-
-          KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,CYR_EXLM,          KC_GRV, KC_MINS,   NDASH,   MDASH, XXXXXXX, XXXXXXX,
-          KC_ENT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,CYR_QUES,         CYR_CLN,  CYR_QT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-          KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        CYR_SCLN, CYR_DQT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                                      TO(0),  KC_SPC,   TO(1),           TO(2), KC_BSPC, XXXXXXX
     )
 
 };
