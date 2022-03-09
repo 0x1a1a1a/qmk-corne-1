53991672eb5945d67098f2e35b66355586d62b9c
inununit <59264318+inununit@users.noreply.github.com>
2022-03-09 02:13:09 +0200

    Change TO to MO

--- keymap.c
+++ keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC,
           KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F,    KC_G,            KC_H,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_QUOT,
           KC_TAB,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RBRC,
-                                    XXXXXXX,  KC_SPC,   TO(1),           MO(2), KC_BSPC,   TO(3)
+                                    XXXXXXX,  KC_SPC,   MO(1),           MO(2), KC_BSPC,   MO(3)
 
     ),
 
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC, XXXXXXX,   SPC_L,   KC_UP,   SPC_R,   ALFRD,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     DND, KC_MUTE,
           KC_ENT,   MCTRL, KC_LEFT, KC_DOWN, KC_RGHT,   EMOJI,         XXXXXXX,  HOME_J,  HOME_K,  HOME_L, HOME_SC, KC_VOLU,
           KC_TAB,    DSK1,    DSK2,    DSK3,    DSK4, LANG_US,         XXXXXXX, XXXXXXX, XXXXXXX,  SS_OPT,   ONEPS, KC_VOLD,
-                                      TO(0),  KC_SPC, XXXXXXX,           MO(2), KC_BSPC,   TO(3)
+                                    XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX
 
     ),
 
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_7,    KC_8,    KC_9, KC_PLUS, KC_MINS,  KC_DOT,
           KC_ENT,  HOME_A,  HOME_S,  HOME_D,  HOME_F, XXXXXXX,            KC_4,    KC_5,    KC_6, KC_ASTR, KC_SLSH, KC_COMM,
           KC_TAB, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            KC_1,    KC_2,    KC_3, XXXXXXX, XXXXXXX,  KC_EQL,
-                                      TO(0),  KC_SPC,   TO(1),           MO(2), KC_BSPC,    KC_0
+                                    XXXXXXX, XXXXXXX, XXXXXXX,            KC_0, XXXXXXX, XXXXXXX
 
     )
 
