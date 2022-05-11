712cb4d4684486763a1b0d4375a3923c7cae370a
inununit <59264318+inununit@users.noreply.github.com>
2022-05-11 19:18:26 +0300

    Remove unused special characters

--- keymap.c
+++ keymap.c
@@ -1,14 +1,6 @@
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-/*
-
-Next steps
-      - Set up direct language layout switches (currently: next/previous in list)
-      - 
-      - 
-
-*/
 
 // Home row mods (as Mod-Taps)
 #define HOME_A   LCTL_T(KC_A)
@@ -41,40 +33,7 @@ Next steps
 #define LANG_UA  KC_F17 // macOS: Set input source to Ukrainian
 
 
-#define RDBRC_L  LSFT(KC_9) // Left round bracket
-#define RDBRC_R  LSFT(KC_0) // Right round bracket
-#define UNDRSC   LSFT(KC_MINS) // Underscore
-#define NDASH    LOPT(KC_MINS) // En dash
-#define MDASH    LSFT(LOPT(KC_MINS)) // Em dash
-#define D_QUOT   LSFT(KC_QUOT) // Double quote
-#define COLON    LSFT(KC_SCLN) // Colon
-#define ATSGN    LSFT(KC_2) // At sign
-#define NMBR     LSFT(KC_3) // Number sign
-#define DLLR     LSFT(KC_4) // Dollar sign
-#define PRCNT    LSFT(KC_5) // Percent sign
-#define CARET    LSFT(KC_6) // Caret
-#define AMPRD    LSFT(KC_7) // Ampersand
-#define TILDE    LSFT(KC_GRV) // Tilde
-
-
 #define SS_OPT   LSFT(LCMD(KC_5)) // macOS: Screenshot and recording options
-// #define SS_C_A   LCTL(LSFT(LCMD(KC_4))) // macOS: Copy picture of selected area to the clipboard
-// #define SS_C_S   LCTL(LSFT(LCMD(KC_3))) // macOS: Copy picture of screen to the clipboard
-// #define SS_F_A   LSFT(LCMD(KC_4)) // macOS: Save picture of selected area as a file
-// #define SS_F_S   LSFT(LCMD(KC_3)) // macOS: Save picture of screen as a file
-
-
-// #define CYR_CM   LSFT(KC_SLSH) // Cyrillic: Comma
-// #define CYR_PD   KC_SLSH // Cyrillic: Period
-// #define CYR_E    KC_QUOT // Cyrillic: E
-// #define CYR_KH   KC_LBRC // Cyrillic: Kh
-// #define CYR_II   KC_RBRC // Cyrillic: Hard sign
-#define CYR_EXLM LSFT(KC_1) // Cyrillic: Exclamation mark
-#define CYR_QUES LSFT(KC_7) // Cyrillic: Question mark
-#define CYR_CLN  LSFT(KC_6) // Cyrillic: Colon
-#define CYR_SCLN LSFT(KC_4) // Cyrillic: Semicolon
-#define CYR_QT   LOPT(KC_P) // Cyrillic: Quote
-#define CYR_DQT  LSFT(KC_2) // Cyrillic: Double quote
 
 
 // #define CMDZ     LCMD(KC_Z) // Redo
