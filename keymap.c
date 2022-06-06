848e0a4af814dbfc941fb1333bad094b3087df28
inununit <59264318+inununit@users.noreply.github.com>
2022-06-06 16:09:00 +0300

    Update the numpad on Layer 2

--- keymap.c
+++ keymap.c
@@ -75,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // NUMPAD       
     [2] = LAYOUT_split_3x6_3(
 
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_MINS,    KC_7,    KC_8,    KC_9,    SLSH, KC_PPLS,
-         XXXXXXX, KC_LCTL, KC_LSFT, KC_LOPT, KC_LCMD, XXXXXXX,            KC_0,    KC_4,    KC_5,    KC_6,  KC_ENT, KC_QUOT,
-         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_PLUS,    KC_1,    KC_2,    KC_3, KC_ASTR,  KC_EQL,
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_PLUS,    KC_4,    KC_5,    KC_6, KC_ASTR, KC_QUOT,
+         XXXXXXX, KC_LCTL, KC_LSFT, KC_LOPT, KC_LCMD, XXXXXXX,            KC_0,    KC_1,    KC_2,    KC_3,  KC_ENT, KC_PPLS,
+         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         KC_MINS,    KC_7,    KC_8,    KC_9,    SLSH,  KC_EQL,
                                     LANG_EN,  KC_SPC,   TO(1),           TO(3), KC_BSPC, LANG_UA
 
     ),
