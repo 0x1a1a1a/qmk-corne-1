8980806a20b06740b101ce45ce44e4364c550ad5
inununit <59264318+inununit@users.noreply.github.com>
2023-03-02 03:43:38 +0200

    Added direct switching between desktops/spaces

--- keymap/combos.c
+++ keymap/combos.c
@@ -6,13 +6,13 @@
 const uint16_t PROGMEM arrows[] = {KC_W, KC_E, COMBO_END};
 // const uint16_t PROGMEM en[] = {KC_E, KC_R, COMBO_END};
 const uint16_t PROGMEM navigation[] = {KC_E, KC_R, COMBO_END};
-const uint16_t PROGMEM special[] = {KC_Z, KC_X, COMBO_END};
+const uint16_t PROGMEM special[] = {KC_-C, KC_V, COMBO_END};
 // const uint16_t PROGMEM ua[] = {KC_C, KC_V, COMBO_END};
 
 
 // Right half
-const uint16_t PROGMEM numpad[] = {KC_M, KC_KC_COMMA, COMBO_END};
-const uint16_t PROGMEM games[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM numpad[] = {KC_M, KC_COMMA, COMBO_END};
+const uint16_t PROGMEM nomodtap[] = {KC_U, KC_I, COMBO_END};
 
 
 
@@ -22,5 +22,5 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(numpad, TO(_NUM)), // Switch to Layer: NUM
     COMBO(navigation, TO(_NAV)), // Switch to Layer: NAV
     COMBO(special, TO(_SPE)), // Switch to Layer: SPE
-    COMBO(games, TO(_GAM)), // Switch to Layer: GAM
+    COMBO(nomodtap, TO(_NMT)), // Switch to Layer: NMT
 };
